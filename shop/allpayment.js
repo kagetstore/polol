05524574a32d0078a72960debbaf761737feacac
kagetstore <87493595+kagetstore@users.noreply.github.com>
2021-12-09 18:18:28 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,21 +1,26 @@
 const allpayment = (prefix) => {
 return ` `PAYMENT BY ZEEONE OFC
 
-*🏧 BANK*
 
+*🏧 BANK*
 1. BRI 
-	ㅁ 8881 0887 4350 47326 = 
+	ㅁ 8881 0887 4350 47326
+2. BCA 
+	ㅁ 3901 0887 4350 47326
+3. BNI 
+	ㅁ 881 0887 4350 47326
+4. PERMATA
+	ㅁ 852 8887 4350 47326
 	
 *💰 E-MONEY*
-
-GOPAY
-	ㅁ 0812 7930 1794
+1. GOPAY
+	ㅁ 0887 4350 47326
 2. OVO
-	ㅁ 0857 6350 0823
+	ㅁ 0887 4350 47326
 3. DANA
-	ㅁ 0857 6350 0823
+	ㅁ 0887 4350 47326
 	
-Semuanya atas nama KA******RE
+Semuanya atas nama RI****DI
 *📲 PULSA*
 Tidak tersedia
 	
@@ -23,5 +28,3 @@ Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
 	}
 
-exports.allpayment = allpayment
- 
